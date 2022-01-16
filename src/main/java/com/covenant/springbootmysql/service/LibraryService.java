d0a9cbaca8d100cbb2afb43ce3931d58f1327e5b
kimdaehee90 <rotnprgl432@gamil.com>
2022-01-16 12:40:39 +0900

    Feat: 추가 lendABook  함수
    아직 이해 못함..

--- src/main/java/com/covenant/springbootmysql/service/LibraryService.java
+++ src/main/java/com/covenant/springbootmysql/service/LibraryService.java
@@ -1,10 +1,9 @@
 package com.covenant.springbootmysql.service;
 
-import com.covenant.springbootmysql.model.Author;
-import com.covenant.springbootmysql.model.Book;
-import com.covenant.springbootmysql.model.Member;
+import com.covenant.springbootmysql.model.*;
 import com.covenant.springbootmysql.model.request.AuthorCreationRequest;
 import com.covenant.springbootmysql.model.request.BookCreationRequest;
+import com.covenant.springbootmysql.model.request.BookLendRequest;
 import com.covenant.springbootmysql.model.request.MemberCreationRequest;
 import com.covenant.springbootmysql.repository.AuthorRepository;
 import com.covenant.springbootmysql.repository.BookRepository;
@@ -15,6 +14,9 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityNotFoundException;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -96,5 +98,50 @@ public class LibraryService {
         Author author = new Author();
         BeanUtils.copyProperties(request, author);
         return authorRepository.save(author);
+    }
+
+    // BookLendRequest로 도서를 대출
+    public List<String> lendABook (List<BookLendRequest> list) {
+        List<String> booksApprovedToBurrow = new ArrayList<>();
+        list.forEach(bookLendRequest -> {
+            // bookLendRequest에 bookIds로 되어 있는데 어떻게 바로 getBookId가 되는지 자동 완성도
+            // 되지 않는데 이해가 잘 안되네요
+            Optional<Book> bookForId =
+                    bookRepository.findById(bookLendRequest.getBookId);
+            if (!bookForId.isPresent()) {
+                throw new EntityNotFoundException(
+                        "Cant find any book under given ID");
+            }
+
+            Optional<Member> memberForId =
+                    memberRepository.findById(bookLendRequest.getMemberId());
+            if (!memberForId.isPresent()) {
+                throw new EntityNotFoundException(
+                        "Member not present in the database");
+            }
+
+            Member member = memberForId.get();
+            if (member.getStatus() != MemberStatus.ACTIVE) {
+                throw new RuntimeException(
+                        "User is not active to proceed a lending.");
+            }
+
+            Optional<Lend> burrowedBook =
+                    lendRepository.findByBookAndStatus(
+                            bookForId.get(), LendStatus.BURROWED);
+
+            if (!burrowedBook.isPresent()) {
+                booksApprovedToBurrow.add(bookForId.get().getName());
+                Lend lend = new Lend();
+                lend.setMember(memberForId.get());
+                lend.setBook(bookForId.get());
+                lend.setStatus(LendStatus.BURROWED);
+                lend.setStartOn(Instant.now());
+                lend.setDueOn(Instant.now().plus(30, ChronoUnit.DAYS));
+                lendRepository.save(lend);
+            }
+        });
+
+        return booksApprovedToBurrow;
     }
 }
