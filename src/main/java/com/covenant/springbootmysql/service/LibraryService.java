e07a3893fd24685a003be9dcd0fdb2c2e146f408
kimdaehee90 <rotnprgl432@gamil.com>
2022-01-16 10:28:18 +0900

    Feat: 추가 readBooks 함수
    모든 book 조회

--- src/main/java/com/covenant/springbootmysql/service/LibraryService.java
+++ src/main/java/com/covenant/springbootmysql/service/LibraryService.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityNotFoundException;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -28,5 +29,10 @@ public class LibraryService {
         }
         throw new EntityNotFoundException(
                 "Cant find any book under given ID");
+    }
+
+    // 모든 book 조회
+    public List<Book> readBooks(){
+        return bookRepository.findAll();
     }
 }
